a73b337409c50f58a95603c3db3b11fcc1199ca8
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2022-12-20 21:12:22 +0600

    added agency name in api/projects/all response

--- repository/projects.js
+++ repository/projects.js
@@ -1,9 +1,10 @@
-const {getConnection} = require('../config/database');
+const { getConnection } = require('../config/database');
 
-class ProjectRepository{
-    findAll = async function (autoCommit = true){
+class ProjectRepository {
+    findAll = async function (autoCommit = true) {
         const client = await getConnection()
-        const data = await client.query('SELECT * FROM projects');
+        const data = await client.query(`SELECT projects.*, agencies.name as agency FROM projects
+join agencies on(projects."exec" = agencies.code);`);
         client.release();
         return data.rows
     }
